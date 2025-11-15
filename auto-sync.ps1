d3b768d35d23b127a5181d96a47bb5c0cd8f7782
cojtinyadiel-code <cojtinyadiel@gmail.com>
2025-11-15 15:36:55 -0600

    msg

--- auto-sync.ps1
+++ auto-sync.ps1
@@ -1,3 +1,6 @@
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$OutputEncoding = [System.Text.Encoding]::UTF8
+
 $repoPath = Split-Path -Parent $MyInvocation.MyCommand.Path
 Set-Location $repoPath
 
