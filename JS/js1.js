9b6413e373ffe2d4fc8233b3a208a6a660f9c537
cojtinyadiel-code <cojtinyadiel@gmail.com>
2025-11-16 09:20:55 -0600

    msg

--- JS/js1.js
+++ JS/js1.js
@@ -1,5 +1,5 @@
 function filtrar(){
-    let texto= document.getElementById("filtro").ariaValueMax.toLowerCase();
+    let texto= document.getElementById("filtro").value.toLowerCase();
     let prodcutos= DocumentFragment.getElementById("productos").value;
     let tarjetas=document.querySelectorAll("tarjeta-producto");
 
@@ -8,38 +8,18 @@ function filtrar(){
         let titulo=card.querySelector("h3.titulo").textContent.tolosercase();
         let cat=card.getAttribute("data-categoria");
         let concideTexto = titulo.includes(texto);
-        let concideCategoria 
-    }
+        let concideCategoria = (categoria === "todas" || categoria=== cat);
+
+        if( concideTexto && concidecategoria){
+            card.style.display="flex";
+        }
+        else{
+            card.style.display="nome";
+        }
+    });
+
+   
 
 
-
-
-
-
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
+    
